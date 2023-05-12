1a4a4cea34b0bdee94c19399345e8e5d24855f8e
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-05-12 15:12:17 +0200

    ПР14: автотесты

--- controllers/users.js
+++ controllers/users.js
@@ -102,7 +102,7 @@ module.exports.updateUserAvatar = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body
 
-  return User.findUserByCredentials(email, password)
+  User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'key')
 
