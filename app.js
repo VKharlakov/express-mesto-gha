b8c8eef7ebdd76719e51d9da4c60cb6698aeaa50
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-05-12 13:52:43 +0200

    ПР14: автотесты

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate')
 const { login, createUser } = require('./controllers/users')
 const { auth } = require('./middlewares/auth');
@@ -10,6 +11,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 mongoose.connect('mongodb://127.0.0.1/mestodb')
   .then(() => console.log('Успешное подключение к MongoDB'))
