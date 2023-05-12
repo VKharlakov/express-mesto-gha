8d27c7ce3d64b4a56c35a437d7fc1485fb980927
Vitaly Kharlakov <vkharlakov@yandex.ru>
2023-05-12 18:10:06 +0200

    ПР14: автотест

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const isEmail = require('validator/lib/isEmail');
+const Unauthorized = require('../errors/Unauthorized');
+
 
 const userSchema = mongoose.Schema({
   name: {
